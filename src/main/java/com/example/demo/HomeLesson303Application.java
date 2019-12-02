c78d353144398bc7757190cdd7b935f1e75308fa
Soheila Escobar <soheilaescobar@Soheilas-MacBook-Pro.local>
2019-12-02 14:32:43 -0500

    SB303

---
+++ src/main/java/com/example/demo/HomeLesson303Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLesson303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeLesson303Application.class, args);
+    }
+
+}
